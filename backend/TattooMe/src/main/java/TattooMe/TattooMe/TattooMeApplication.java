c4ca0194eb95026edec85ace41cac186fcdb7417
S25088 <S25088@pjwstk.edu.pl>
2025-03-28 09:41:01 +0100

    usuniecie submodulow backend

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/TattooMeApplication.java
@@ -0,0 +1,13 @@
+package TattooMe.TattooMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TattooMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TattooMeApplication.class, args);
+	}
+
+}
---
+++ backend/TattooMe/src/test/java/TattooMe/TattooMe/TattooMeApplicationTests.java
@@ -0,0 +1,13 @@
+package TattooMe.TattooMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TattooMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
