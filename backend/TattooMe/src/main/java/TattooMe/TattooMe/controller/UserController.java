b9b65500881802b9c919ff9a562b0ef412e86d0e
S25088 <S25088@pjwstk.edu.pl>
2025-04-10 11:23:28 +0200

    Dodanie user service i wyszukiwanie uzytkownikow

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/controller/UserController.java
@@ -0,0 +1,26 @@
+package TattooMe.TattooMe.controller;
+
+import TattooMe.TattooMe.entity.User;
+import TattooMe.TattooMe.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.findAllUsers();
+    }
+}
