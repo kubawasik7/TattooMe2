e2890f6569a84eceadbece188382fe2f3037c3aa
S25088 <S25088@pjwstk.edu.pl>
2025-04-10 11:53:06 +0200

    implementacja user list component

--- frontend/TattooMe/src/app/component/user-list/user-list.component.ts
+++ frontend/TattooMe/src/app/component/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../service/user.service';
 
 @Component({
   selector: 'app-user-list',
@@ -6,6 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit{
+  users: User[] = [];
+  constructor(private userService: UserService){}
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+  loadUsers(): void {
+    this.userService.getUsers().subscribe(data =>{
+      this.users = data;
+    });
+  }
 
 }
