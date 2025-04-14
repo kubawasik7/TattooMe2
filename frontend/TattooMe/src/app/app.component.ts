2addd43c10cc01fdf6114854fcd0cf93687ed732
S25088 <S25088@pjwstk.edu.pl>
2025-04-14 11:00:18 +0200

    implementacja metody do przekierowania logowania

--- frontend/TattooMe/src/app/app.component.ts
+++ frontend/TattooMe/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TattooMe';
+  constructor(private router: Router) { }
+
+  onLoginClick(): void {
+    console.log('Przycisk logowania został kliknięty.');
+        this.router.navigate(['/login']);
+  }
+
+  
+  navigateTo(path: string): void {
+    this.router.navigate([`/${path}`]);
+  }
 }
