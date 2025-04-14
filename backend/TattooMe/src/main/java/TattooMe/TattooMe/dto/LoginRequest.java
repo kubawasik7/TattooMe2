fa565672adb7952ab3638be842be13c2006caa0b
S25088 <S25088@pjwstk.edu.pl>
2025-04-14 09:58:48 +0200

    dto dla logowania

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/LoginRequest.java
@@ -0,0 +1,22 @@
+package TattooMe.TattooMe.dto;
+
+public class LoginRequest {
+    private String nickname;
+    private String password;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
