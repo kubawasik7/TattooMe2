cd7b0866049fd19f9635ac4e35a451986cc2142e
S25088 <S25088@pjwstk.edu.pl>
2025-04-14 10:19:45 +0200

    Implementacja logowania

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/controller/AuthController.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/controller/AuthController.java
@@ -1,9 +1,18 @@
 package TattooMe.TattooMe.controller;
 
+import TattooMe.TattooMe.dto.LoginRequest;
+import TattooMe.TattooMe.dto.LoginResponse;
 import TattooMe.TattooMe.dto.RegisterRequest;
+import TattooMe.TattooMe.entity.User;
 import TattooMe.TattooMe.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,14 +20,51 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
     private final UserService userService;
+    private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
     @Autowired
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService,
+                          AuthenticationManager authenticationManager,
+                          JwtUtil jwtUtil,
+                          PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.jwtUtil = jwtUtil;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+
     }
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody RegisterRequest registerRequest) {
         userService.registerUser(registerRequest);
         return ResponseEntity.ok("User registered");
+    }
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) {
+        try {
+
+            User user = userService.getByNickname(loginRequest.getNickname());
+            //To jest do testu - do wywalenia potem
+            if(user == null) {
+                System.out.println("Nie znaleziono użytkownika");
+            } else {
+                System.out.println("Znaleziono użytkownika: " + user);
+            }
+            if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+            }
+
+            CustomUserDetails userDetails = new CustomUserDetails(user);
+            UsernamePasswordAuthenticationToken authToken =
+                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+            
+            String token = jwtUtil.generateToken(userDetails);
+            return ResponseEntity.ok(new LoginResponse(token));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        }
     }
 
 }
