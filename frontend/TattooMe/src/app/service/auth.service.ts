3aace60ca8d2b4e319b9ed43fdad899956999cdf
S25088 <S25088@pjwstk.edu.pl>
2025-04-14 10:59:17 +0200

    buttony dla logowania i rejestracji

--- frontend/TattooMe/src/app/service/auth.service.ts
+++ frontend/TattooMe/src/app/service/auth.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RegisterRequest } from '../model/register-request';
+import { LoginRequest } from '../model/login-request';
+import { LoginResponse } from '../model/login-response';
+import {jwtDecode} from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/auth';
+
+  constructor(private http: HttpClient) { }
+  getToken(): string | null {
+    return (typeof window !== 'undefined') ? localStorage.getItem('token') : null;
+  }
+
+  getUserRole(): string | null {
+    const token = this.getToken();
+    if(!token) return null;
+
+    const decodedToken: any = jwtDecode(token);
+    return decodedToken.role;
+  }
+
+  register(registerRequest: RegisterRequest): Observable<any>{
+    return this.http.post(`${this.baseUrl}/register`, registerRequest);
+  }
+  login(loginRequest: LoginRequest): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.baseUrl}/login`, loginRequest);
+  }
 }
+
+
