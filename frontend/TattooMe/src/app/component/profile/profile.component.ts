8d58223b3b1bfc338fdd797e17ee61d119b5d35e
S25088 <S25088@pjwstk.edu.pl>
2025-04-15 11:15:27 +0200

    pobranie usera po id

--- frontend/TattooMe/src/app/component/profile/profile.component.ts
+++ frontend/TattooMe/src/app/component/profile/profile.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User, UserService } from '../../service/user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
+  user!: User;
+  userId!: string;
+
+  constructor(private route: ActivatedRoute,
+    private userService: UserService
+  ){}
+
+  ngOnInit(): void {
+    this.userId = this.route.snapshot.paramMap.get('id')!;
+    if(this.userId){
+      this.userService.getUserById(this.userId).subscribe(data =>{
+        this.user = data;
+      })
+    }
+  }
 
 }
