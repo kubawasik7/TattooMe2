4b36656fcea079e554c3fef9d22c57374744a1fe
S25088 <S25088@pjwstk.edu.pl>
2025-04-18 14:27:33 +0200

    implementacja logowania

--- frontend/TattooMe/src/app/component/login/login.component.ts
+++ frontend/TattooMe/src/app/component/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { LoginRequest } from '../../model/login-request';
 import { AuthService } from '../../service/auth.service';
 import { Router } from '@angular/router';
+import { FormGroup,FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -14,13 +15,26 @@ export class LoginComponent {
     nickname: '',
     password: ''
   };
-  constructor(private authService: AuthService, private router: Router) { }
+  form: FormGroup;
+
+  constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    })
+   }
 
   onLogin(): void {
+    this.loginRequest = {
+      nickname: this.form.value.username,
+      password: this.form.value.password
+    }
     this.authService.login(this.loginRequest).subscribe(
       response => {
         console.log('Logowanie powiodło się:', response);
+        // Zapisz token np. w localStorage, aby później używać przy kolejnych zapytaniach
         localStorage.setItem('token', response.token);
+        // Przekierowanie po zalogowaniu
         this.router.navigate(['/dashboard']);
       },
       error => {
