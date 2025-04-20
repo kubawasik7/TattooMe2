8e92d0627b5952f58e1ccf3f34f2fd0cd9d96f1a
S25088 <S25088@pjwstk.edu.pl>
2025-04-20 21:20:58 +0200

    Pobranie tatuatorów

--- frontend/TattooMe/src/app/component/tattoo-artist-list/tattoo-artist-list.component.ts
+++ frontend/TattooMe/src/app/component/tattoo-artist-list/tattoo-artist-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User, UserService } from '../../service/user.service';
 
 @Component({
   selector: 'app-tattoo-artist-list',
@@ -7,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './tattoo-artist-list.component.css'
 })
 export class TattooArtistListComponent {
+  users: User[] = [];
 
+  constructor(private userService: UserService){}
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers(): void {
+    this.userService.getTattooArtists().subscribe(data => {
+      this.users = data;
+    });
+  }
 }
