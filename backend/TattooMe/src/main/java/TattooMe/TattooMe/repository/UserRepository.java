9f332816d7260468239cad3204fd48b06cc3a8b8
S25088 <S25088@pjwstk.edu.pl>
2025-04-20 21:13:03 +0200

    Wyszukanie z bazy po roli

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/repository/UserRepository.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/repository/UserRepository.java
@@ -4,10 +4,12 @@ import TattooMe.TattooMe.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByNickname(String nickname);
+    List<User> findByRole(String role);
 }
