6caeddf2701297a5bfc7fe4ea3275178c3a15f60
S25088 <S25088@pjwstk.edu.pl>
2025-07-23 15:42:55 +0200

    dto dla opisu profilu

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/DescriptionProfileDTO.java
@@ -0,0 +1,13 @@
+package TattooMe.TattooMe.dto;
+
+public class DescriptionProfileDTO {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
