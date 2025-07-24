5e4996cd9a1bdb8653857e8b2cc72114256dca11
S25088 <S25088@pjwstk.edu.pl>
2025-07-24 17:07:20 +0200

    tattooArtistOffer entity

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/entity/TattooArtistOffer.java
@@ -0,0 +1,44 @@
+package TattooMe.TattooMe.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tattoo_artist_offer")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TattooArtistOffer {
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @JdbcTypeCode(SqlTypes.BINARY)
+    @Column(
+            name = "tattoo_artist_offer_id",
+            columnDefinition = "BINARY(16)",
+            nullable = false,
+            updatable = false
+    )
+    private UUID id;
+    @Column(name = "start_date")
+    private LocalDateTime startDate;
+    @Column(name = "end_date")
+    private LocalDateTime endDate;
+    @Column(name = "description", columnDefinition = "TEXT", nullable = false)
+    private String description;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User tattooArtist;
+
+
+}
