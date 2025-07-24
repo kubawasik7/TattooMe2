d80a0ae5df9582a056ce4908478bf17afbd3fd6e
S25088 <S25088@pjwstk.edu.pl>
2025-07-24 17:08:24 +0200

    Dto dla tworzenia oferty

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/CreateOfferDTO.java
@@ -0,0 +1,33 @@
+package TattooMe.TattooMe.dto;
+
+import java.time.LocalDateTime;
+
+public class CreateOfferDTO {
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String description;
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
