adb28725b0e7008cdeec01d964b0d589548cc8af
S25088 <S25088@pjwstk.edu.pl>
2025-07-24 17:10:55 +0200

    impl controllera

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/controller/TattooArtistOfferController.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/controller/TattooArtistOfferController.java
@@ -1,11 +1,47 @@
 package TattooMe.TattooMe.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import TattooMe.TattooMe.Security.CustomUserDetails;
+import TattooMe.TattooMe.dto.CreateOfferDTO;
+import TattooMe.TattooMe.dto.OfferDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.AccessDeniedException;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/offers")
 @CrossOrigin(origins = "http://localhost:4200")
 public class TattooArtistOfferController {
+    @Autowired
+    private TattooArtistOfferService service;
+
+    @GetMapping
+    public List<OfferDTO> list(@AuthenticationPrincipal CustomUserDetails user) {
+        return service.getOffers(user.getId());
+    }
+
+    @PostMapping
+    public OfferDTO create(@AuthenticationPrincipal CustomUserDetails user,
+                           @RequestBody CreateOfferDTO dto) {
+        return service.createOffer(user.getId(), dto);
+    }
+
+    @PutMapping("/{id}")
+    public OfferDTO update(@AuthenticationPrincipal CustomUserDetails user,
+                           @PathVariable UUID id,
+                           @RequestBody CreateOfferDTO dto) throws AccessDeniedException {
+        return service.updateOffer(user.getId(), id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@AuthenticationPrincipal CustomUserDetails user,
+                       @PathVariable UUID id) throws AccessDeniedException {
+        service.deleteOffer(user.getId(), id);
+    }
+
 }
