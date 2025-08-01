73f59360e2859f2dbfd650e22dfa5a1ddb76910b
S25088 <S25088@pjwstk.edu.pl>
2025-08-01 19:45:14 +0200

    encja dla portfolio

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/entity/Portfolio.java
@@ -0,0 +1,50 @@
+package TattooMe.TattooMe.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "portfolio")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Portfolio {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @JdbcTypeCode(SqlTypes.BINARY)
+    @Column(
+            name = "portfolio_id",
+            columnDefinition = "BINARY(16)",
+            nullable = false,
+            updatable = false
+    )
+    private UUID id;
+
+    @Lob
+    @Column(name = "picture", nullable = false)
+    private byte[] picture;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        if (createdAt == null) createdAt = Instant.now();
+    }
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false, columnDefinition = "BINARY(16)")
+    private User user;
+}
