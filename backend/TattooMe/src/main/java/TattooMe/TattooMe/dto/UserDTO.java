6c945126877e75622ddd3686087a357868fc7d61
S25088 <S25088@pjwstk.edu.pl>
2025-08-05 13:52:58 +0200

    DTO dla update profilu

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/UserDTO.java
@@ -0,0 +1,51 @@
+package TattooMe.TattooMe.dto;
+
+import java.util.UUID;
+
+public class UserDTO {
+    private UUID id;
+    private String username;
+    private String name;
+    private String surname;
+    private String email;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
