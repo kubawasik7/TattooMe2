55357b906952d6b47016e34250f738187cadb658
S25088 <S25088@pjwstk.edu.pl>
2025-08-06 12:24:41 +0200

    implementacja user-info.component

--- frontend/TattooMe/src/app/component/user-info/user-info.component.ts
+++ frontend/TattooMe/src/app/component/user-info/user-info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-info',
@@ -6,6 +6,41 @@ import { Component } from '@angular/core';
   templateUrl: './user-info.component.html',
   styleUrl: './user-info.component.css'
 })
-export class UserInfoComponent {
+export class UserInfoComponent implements OnInit {
+  info: any = {};
+  originalInfo: any = {};
+  editMode = false;
+
+  constructor(private infoService: UserInfoService) {}
+
+  ngOnInit(): void {
+    this.infoService.getInfo().subscribe(data => {
+      console.log('Dane z backendu:', data);
+      this.info = { ...data };
+      console.log('info:', this.info);
+
+      this.originalInfo = { ...data };
+    });
+  }
+
+  enableEdit(): void {
+    this.editMode = true;
+  }
+
+  saveChanges(): void {
+    this.infoService.updateInfo(this.info).subscribe({
+      next: () => {
+        this.originalInfo = { ...this.info };
+        this.editMode = false;
+        alert('Zapisano informacje');
+      },
+      error: () => alert('Błąd')
+    });
+  }
+
+  cancelChanges(): void {
+    this.info = { ...this.originalInfo };
+    this.editMode = false;
+  }
 
 }
