2814ee995bdb462f312daece39e9fce8fc1ca892
S25088 <S25088@pjwstk.edu.pl>
2025-08-06 12:16:43 +0200

    dodanie adnotacji dla repository

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/repository/PersonInfoRepository.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/repository/PersonInfoRepository.java
@@ -2,10 +2,11 @@ package TattooMe.TattooMe.repository;
 
 import TattooMe.TattooMe.entity.PersonInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
-
+@Repository
 public interface PersonInfoRepository extends JpaRepository<PersonInfo, UUID> {
     Optional<PersonInfo> findByUser_Id(UUID userId);
 
