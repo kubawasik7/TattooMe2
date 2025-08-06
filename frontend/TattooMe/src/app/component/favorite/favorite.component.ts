1b82af821c8074640ecd853d31abfe3eec6b48f4
S25088 <S25088@pjwstk.edu.pl>
2025-08-06 14:47:31 +0200

    implementacja favorite.component

--- frontend/TattooMe/src/app/component/favorite/favorite.component.ts
+++ frontend/TattooMe/src/app/component/favorite/favorite.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FavoriteService } from '../../service/favorite.service';
+import { User } from '../../service/user.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-favorite',
@@ -6,6 +10,27 @@ import { Component } from '@angular/core';
   templateUrl: './favorite.component.html',
   styleUrl: './favorite.component.css'
 })
-export class FavoriteComponent {
+export class FavoriteComponent implements OnInit{
+favorites: User[] = [];
 
+  constructor(private favoriteService: FavoriteService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadFavorites();
+  }
+
+  loadFavorites(): void {
+    this.favoriteService.getFavorites().subscribe(data => {
+      this.favorites = data;
+    });
+  }
+
+  removeFavorite(artistId: string): void {
+    this.favoriteService.removeFavorite(artistId).subscribe(() => {
+      this.loadFavorites();
+    });
+  }
+  goToArtistProfile(artistId: string): void {
+  this.router.navigate(['/userProfile', artistId]);
+}
 }
