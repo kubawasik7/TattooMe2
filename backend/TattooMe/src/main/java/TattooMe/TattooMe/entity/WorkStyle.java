324014b15f0b617a779b9051d0379f686f7f7765
S25088 <S25088@pjwstk.edu.pl>
2025-08-06 21:29:14 +0200

    dodanie encji WorkStyle

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/entity/WorkStyle.java
@@ -0,0 +1,40 @@
+package TattooMe.TattooMe.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "work_style")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WorkStyle {
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @JdbcTypeCode(SqlTypes.BINARY)
+    @Column(
+            name = "work_style_id",
+            columnDefinition = "BINARY(16)",
+            nullable = false,
+            updatable = false
+    )
+    private UUID id;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "tattoo_style_id")
+    private TattooStyle tattooStyle;
+
+}
