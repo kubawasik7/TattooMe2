edb1faf380100b197b20ad269b8d2659f44d6bec
S25088 <S25088@pjwstk.edu.pl>
2025-08-06 14:46:13 +0200

    implementacja favorite service

--- frontend/TattooMe/src/app/service/favorite.service.ts
+++ frontend/TattooMe/src/app/service/favorite.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from './user.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoriteService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/favorites';
+
+  constructor(private http: HttpClient) {}
+
+  getFavorites(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl);
+  }
+
+  addFavorite(artistId: string): Observable<void> {
+    return this.http.post<void>(`${this.baseUrl}/${artistId}`, {});
+  }
+
+  removeFavorite(artistId: string): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${artistId}`);
+  }
 }
