5567963354ffd612a3f8c338343f027043775796
S25088 <S25088@pjwstk.edu.pl>
2025-08-08 15:38:22 +0200

    dodanie portfolioService

---
+++ frontend/TattooMe/src/app/service/portfolio.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Portfolio } from '../model/portfolio';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PortfolioService {
+  private baseUrl = 'http://localhost:8080/api/portfolio';
+
+  constructor(private http: HttpClient) { }
+
+  getByUser(userId: string): Observable<Portfolio[]> {
+    return this.http.get<Portfolio[]>(`${this.baseUrl}/${userId}`);
+  }
+
+  uploadImage(file: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post<Portfolio>(`${this.baseUrl}/upload`, formData);
+  }
+
+  delete(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+}
