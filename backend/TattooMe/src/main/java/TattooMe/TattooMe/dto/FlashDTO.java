38765c2a96469c73df4f4e6fdcd67b3fb7b6a02b
S25088 <S25088@pjwstk.edu.pl>
2025-08-08 22:36:53 +0200

    dodanie DTO Flash

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/FlashDTO.java
@@ -0,0 +1,10 @@
+package TattooMe.TattooMe.dto;
+
+public class FlashDTO {
+    public Integer sizeMin;
+    public Integer sizeMax;
+    public Integer priceMin;
+    public Integer priceMax;
+    public String  reccomendedPlace;
+    public String  description;
+}
