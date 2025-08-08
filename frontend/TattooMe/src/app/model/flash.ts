b0b4d7b09e52fd757be6aa8b685d66a9a1940909
S25088 <S25088@pjwstk.edu.pl>
2025-08-08 22:41:32 +0200

    model dla flasha

---
+++ frontend/TattooMe/src/app/model/flash.ts
@@ -0,0 +1,10 @@
+export interface Flash {
+  id?: string;
+  picture?: string;
+  description: string;
+  sizeMin: number;
+  sizeMax: number;
+  priceMin: number;
+  priceMax: number;
+  reccomendedPlace: string;
+}
