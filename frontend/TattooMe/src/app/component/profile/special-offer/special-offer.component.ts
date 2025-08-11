25ad8b616b922e1221284b5424a720238845bbba
S25088 <S25088@pjwstk.edu.pl>
2025-08-11 18:52:07 +0200

    implementacja promocji

--- frontend/TattooMe/src/app/component/profile/special-offer/special-offer.component.ts
+++ frontend/TattooMe/src/app/component/profile/special-offer/special-offer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CreateOffer, Offer, ProfileService } from '../../../service/profile.service';
 
 @Component({
   selector: 'app-special-offer',
@@ -7,5 +8,50 @@ import { Component } from '@angular/core';
   styleUrl: './special-offer.component.css'
 })
 export class SpecialOfferComponent {
+  offers: Offer[] = [];
+  editingId: string | null = null;
+  draft: CreateOffer = { startDate: '', endDate: '', description: '' };
+
+  constructor(private profileService: ProfileService){}
+
+  ngOnInit(): void{
+    
+  }
+
+  load() {
+    this.profileService.getOffers().subscribe(list => this.offers = list);
+  }
+  startNew() {
+    this.editingId = 'new';
+    this.draft = { startDate: '', endDate: '', description: '' };
+  }
+
+  startEditOffer(o: Offer) {
+    this.editingId = o.id;
+    this.draft = {
+      startDate: o.startDate,
+      endDate: o.endDate,
+      description: o.description
+    };
+  }
+
+  save() {
+    if (this.editingId === 'new') {
+      this.profileService.createOffer(this.draft).subscribe(() => this.load());
+    } else {
+      this.profileService.updateOffer(this.editingId!, this.draft).subscribe(() => this.load());
+    }
+    this.editingId = null;
+  }
+
+  cancel() {
+    this.editingId = null;
+  }
+
+  delete(id: string) {
+    if (confirm('Usunąć tę ofertę?')) {
+      this.profileService.deleteOffer(id).subscribe(() => this.load());
+    }
+  }
 
 }
