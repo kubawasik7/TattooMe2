b614412f33a97f63965103ce5773e138ce45367b
S25088 <S25088@pjwstk.edu.pl>
2025-08-11 19:06:11 +0200

    implementacja portfolio

--- frontend/TattooMe/src/app/component/profile/portfolio/portfolio.component.ts
+++ frontend/TattooMe/src/app/component/profile/portfolio/portfolio.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Portfolio } from '../../../model/portfolio';
+import { PortfolioService } from '../../../service/portfolio.service';
 
 @Component({
   selector: 'app-portfolio',
@@ -7,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrl: './portfolio.component.css'
 })
 export class PortfolioComponent {
+  @Input() userId!: string;
+  portfolioItems: Portfolio[] = [];
+  selectedFile: File | null = null;
 
+  constructor(private portfolioService: PortfolioService){}
+
+  ngOnInit(): void{
+      this.portfolioService.getByUser(this.userId).subscribe(items => {
+      this.portfolioItems = items;
+    });
+  }
+
+  deleteImagePortfolio(id: string) {
+  this.portfolioService.delete(id).subscribe(() => {
+    this.portfolioItems = this.portfolioItems.filter(p => p.id !== id);
+  });
+}
+
+  onFileSelectedPortfolio(event: any): void {
+    if (event.target.files.length > 0) {
+      this.selectedFile = event.target.files[0];
+    }
+  }
+
+  uploadPortfolio(): void {
+    if (!this.selectedFile) return;
+
+    this.portfolioService.uploadImage(this.selectedFile).subscribe({
+      next: () => {
+        this.selectedFile = null;
+      },
+      error: err => console.error('Błąd uploadu portfolio', err)
+    });
+  }
 }
