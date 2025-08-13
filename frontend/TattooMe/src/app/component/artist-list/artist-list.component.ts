705edca1d923cf64896169c56e0cee3d2ce9930e
S25088 <S25088@pjwstk.edu.pl>
2025-08-13 18:34:21 +0200

    implementacja artist list

--- frontend/TattooMe/src/app/component/artist-list/artist-list.component.ts
+++ frontend/TattooMe/src/app/component/artist-list/artist-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User, UserService } from '../../service/user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-artist-list',
@@ -7,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './artist-list.component.css'
 })
 export class ArtistListComponent {
+  role!: 'tattoo_artist' | 'trainee';
+  users: User[] = [];
+  p = 1;
 
+  constructor(private userService: UserService, private activatedRoute: ActivatedRoute) { }
+  ngOnInit(): void {
+    this.role = this.activatedRoute.snapshot.data['role'];
+    this.loadUsers();
+  }
+
+  loadUsers(): void {
+    this.userService.getUsersByRole(this.role).subscribe({
+      next: data => { this.users = data; }
+    })
+  }
 }
