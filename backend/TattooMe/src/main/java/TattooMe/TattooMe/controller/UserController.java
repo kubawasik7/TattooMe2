987c374c07d52516978cbc5f445f03c9b51dc155
S25088 <S25088@pjwstk.edu.pl>
2025-08-13 18:28:41 +0200

    usuniecie metod

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/controller/UserController.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/controller/UserController.java
@@ -25,21 +25,9 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-    @GetMapping
-    public List<User> getAllUsers(){
-        return userService.findAllUsers();
-    }
     @GetMapping("/{id}")
     public ResponseEntity<?> getUserById(@PathVariable UUID id) {
         return ResponseEntity.ok(userService.getUserById(String.valueOf(id)));
-    }
-    @GetMapping("/tattooArtist")
-    public List<User> getAllTattooArtist(){
-        return userService.findAllTattooArtist();
-    }
-    @GetMapping("/trainee")
-    public List<User> getAllTrainee(){
-        return userService.findAllTrainees();
     }
     @PostMapping(
             value = "/avatar",
