475a5f2dc35978a94fec7f025246dba82c4fcf44
S25088 <S25088@pjwstk.edu.pl>
2025-08-15 15:53:12 +0200

    dodanie adnotacji @repository

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/repository/ChatRepository.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/repository/ChatRepository.java
@@ -2,10 +2,12 @@ package TattooMe.TattooMe.repository;
 
 import TattooMe.TattooMe.entity.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
+@Repository
 public interface ChatRepository extends JpaRepository<Chat, UUID> {
     List<Chat> findByInitiator_IdOrReceiver_Id(UUID initiatorId, UUID receiverId);
     Optional<Chat> findByInitiator_IdAndReceiver_Id(UUID initiatorId, UUID receiverId);
