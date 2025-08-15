9abe49e394c7e63214f3cc0f5184720f3534fd2c
S25088 <S25088@pjwstk.edu.pl>
2025-08-15 15:49:28 +0200

    dodanie MessageDTO

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/MessageDTO.java
@@ -0,0 +1,16 @@
+package TattooMe.TattooMe.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class MessageDTO {
+    private UUID id;
+    private String content;
+    private LocalDateTime date;
+    private UUID senderId;
+    private String base64Attachment;
+}
