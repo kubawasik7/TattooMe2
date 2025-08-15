a90d0fd89cd4a6bc21c9608e170adece36b5aba3
S25088 <S25088@pjwstk.edu.pl>
2025-08-15 15:44:57 +0200

    dodanie encji Chat

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/entity/Chat.java
@@ -0,0 +1,42 @@
+package TattooMe.TattooMe.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name="chat")
+@Getter
+@Setter
+public class Chat {
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @JdbcTypeCode(SqlTypes.BINARY)
+    @Column(
+            name = "chat_id",
+            columnDefinition = "BINARY(16)",
+            nullable = false,
+            updatable = false
+    )
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "initiator_id", nullable = false)
+    private User initiator;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    @OneToMany(mappedBy = "chat", cascade = CascadeType.ALL)
+    private List<Message> messages = new ArrayList<>();
+
+}
