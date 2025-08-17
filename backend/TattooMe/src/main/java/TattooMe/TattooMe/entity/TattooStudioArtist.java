5cd774c09e07219de25357346df5931f645c02b6
S25088 <S25088@pjwstk.edu.pl>
2025-08-17 10:10:02 +0200

    dodanie encji posredniej TattooStudioArtist

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/entity/TattooStudioArtist.java
@@ -0,0 +1,25 @@
+package TattooMe.TattooMe.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tattoo_studio_artist")
+@Getter
+@Setter
+@NoArgsConstructor
+@IdClass(TattooStudioArtistId.class)
+public class TattooStudioArtist {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tattoo_studio_id")
+    private TattooStudio tattooStudio;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
