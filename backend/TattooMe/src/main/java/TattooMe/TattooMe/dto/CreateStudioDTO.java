9117f8f7fa289b0a757c764266c49a9e23d0ab70
S25088 <S25088@pjwstk.edu.pl>
2025-08-17 10:04:02 +0200

    dodanie dto dla stworzenia studia

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/CreateStudioDTO.java
@@ -0,0 +1,14 @@
+package TattooMe.TattooMe.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateStudioDTO {
+    private String name;
+    private String city;
+    private String street;
+    private String streetNumber;
+    private String postalCode;
+}
