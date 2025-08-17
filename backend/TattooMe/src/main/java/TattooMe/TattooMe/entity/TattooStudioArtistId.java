664e168e1ff881d13209a83be5dcd0f7c96debde
S25088 <S25088@pjwstk.edu.pl>
2025-08-17 10:11:00 +0200

    dodanie encji klucza zlozonego

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/entity/TattooStudioArtistId.java
@@ -0,0 +1,17 @@
+package TattooMe.TattooMe.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class TattooStudioArtistId implements Serializable {
+    private UUID tattooStudio;
+    private UUID user;
+}
