bc2767de94d02454c4263f5da6519e7171752ca5
S25088 <S25088@pjwstk.edu.pl>
2025-08-21 13:30:22 +0200

    dodanie NewVisitDTO

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/NewVisitDTO.java
@@ -0,0 +1,18 @@
+package TattooMe.TattooMe.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class NewVisitDTO {
+    private UUID artistDateId;
+    private UUID flashId;
+    private String comment;
+    private String allergies;
+    private String chronicDiseases;
+    private String medicines;
+    private String experiences;
+}
