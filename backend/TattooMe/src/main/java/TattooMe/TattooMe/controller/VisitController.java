30ddf4d44e96920a994cfac7c2771924fcbe217c
S25088 <S25088@pjwstk.edu.pl>
2025-08-21 13:34:20 +0200

    dodanie VisitController

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/controller/VisitController.java
@@ -0,0 +1,25 @@
+package TattooMe.TattooMe.controller;
+
+import TattooMe.TattooMe.Security.CustomUserDetails;
+import TattooMe.TattooMe.dto.NewVisitDTO;
+import TattooMe.TattooMe.service.VisitService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/visits")
+@RequiredArgsConstructor
+public class VisitController {
+    private final VisitService visitService;
+    @PostMapping
+    public ResponseEntity<Void> createVisit(@RequestBody NewVisitDTO newVisitDTO, @AuthenticationPrincipal CustomUserDetails user)
+    {
+        visitService.createVisit(newVisitDTO, user.getId());
+        return ResponseEntity.ok().build();
+    }
+}
