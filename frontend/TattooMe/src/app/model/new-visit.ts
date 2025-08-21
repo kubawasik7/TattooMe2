0782f7bb79f41e79d0db87b5db7486b8a9940d2c
S25088 <S25088@pjwstk.edu.pl>
2025-08-21 13:43:57 +0200

    dodanie modelu newVisit

---
+++ frontend/TattooMe/src/app/model/new-visit.ts
@@ -0,0 +1,9 @@
+export interface NewVisit {
+  artistDateId: string;
+  flashId?: string;
+  comment?: string;
+  allergies?: string;
+  chronicDiseases?: string;
+  medicines?: string;
+  experiences?: string;
+}
