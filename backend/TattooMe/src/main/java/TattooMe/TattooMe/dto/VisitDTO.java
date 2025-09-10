6a9751f887f684a1185a13945b07f75891dbdffb
S25088 <S25088@pjwstk.edu.pl>
2025-09-10 11:34:17 +0200

    dodanie pol do wizyty

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/VisitDTO.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/VisitDTO.java
@@ -10,10 +10,15 @@ import java.util.UUID;
 @Setter
 public class VisitDTO {
     private UUID id;
-    private String comment;
     private String status;
     private LocalDateTime date;
     private String artistName;
+    private String clientName;
+    private String comment;
     private String flashDescription;
-    private String clientNickname;
+    private String tattooStudioName;
+    private String allergies;
+    private String chronicDiseases;
+    private String medicines;
+    private String experiences;
 }
