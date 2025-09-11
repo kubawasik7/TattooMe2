4993432803e84e369a3c0eb5182010b10a64cc00
S25088 <S25088@pjwstk.edu.pl>
2025-09-11 12:36:22 +0200

    dodanie metody findByVisit_ID

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/repository/ReviewRepository.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/repository/ReviewRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
     List<Review> findByTarget_Id(UUID userId);
     List<Review> findByTattooStudio_Id(UUID studioId);
+    Optional<Review> findByVisit_Id(UUID visitId);
+
 }
