add0bebcbc06ff8bd6d4f9def905347818307d2d
S25088 <S25088@pjwstk.edu.pl>
2025-09-11 12:36:52 +0200

    dodanie @getter@setter

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/ReviewAnswerDTO.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/ReviewAnswerDTO.java
@@ -2,11 +2,14 @@ package TattooMe.TattooMe.dto;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
-
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ReviewAnswerDTO {
