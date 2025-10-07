4548efc83504ca86183d149624e514e010f1370b
Kuba <jakubwasik752@gmail.com>
2025-10-07 14:17:30 +0200

    Dodanie DTO dla TattoStyle

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/TattooStyleDTO.java
@@ -0,0 +1,18 @@
+package TattooMe.TattooMe.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TattooStyleDTO {
+    private UUID id;
+    private String name;
+}
