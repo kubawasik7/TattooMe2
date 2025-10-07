31c14e60e058eeb8495d15e179a860794f512793
Kuba <jakubwasik752@gmail.com>
2025-10-07 11:12:58 +0200

    edycja ScheduleDTO

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/ScheduleDTO.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/ScheduleDTO.java
@@ -1,35 +1,15 @@
 package TattooMe.TattooMe.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Getter
+@Setter
 public class ScheduleDTO {
     private UUID id;
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public boolean isAvailable() {
-        return available;
-    }
-
-    public void setAvailable(boolean available) {
-        this.available = available;
-    }
-
     private LocalDateTime dateTime;
     private boolean available;
 }
