3698008ea1b2ce52e1a67c14564ca835397f42bb
Kuba <jakubwasik752@gmail.com>
2025-10-07 13:48:29 +0200

    Zabezpieczenie DescriptionProfileDTO

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/DescriptionProfileDTO.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/DescriptionProfileDTO.java
@@ -1,13 +1,14 @@
 package TattooMe.TattooMe.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DescriptionProfileDTO {
+    @NotBlank(message = "Opis nie może być pusty")
+    @Size(max = 2000, message = "Opis nie może mieć więcej niż 2000 znaków")
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
