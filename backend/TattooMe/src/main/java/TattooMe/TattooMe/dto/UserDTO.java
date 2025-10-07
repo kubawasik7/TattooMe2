c1709a821eb4aeece8d855cc70cd91cf727d6ada
Kuba <jakubwasik752@gmail.com>
2025-10-07 11:13:08 +0200

    edycja UserDTO

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/UserDTO.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/UserDTO.java
@@ -1,51 +1,15 @@
 package TattooMe.TattooMe.dto;
 
-import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.UUID;
+@Getter
+@Setter
 public class UserDTO {
     private UUID id;
     private String username;
     private String name;
     private String surname;
     private String email;
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
