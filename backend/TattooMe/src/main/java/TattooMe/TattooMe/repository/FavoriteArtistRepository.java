3027f41f4a1ea0fa9d8a530faf81e0a744fd8630
Kuba <jakubwasik752@gmail.com>
2025-10-08 14:33:46 +0200

    dodanie metody deleteByIserAndArtist

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/repository/FavoriteArtistRepository.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/repository/FavoriteArtistRepository.java
@@ -1,15 +1,20 @@
 package TattooMe.TattooMe.repository;
 
 import TattooMe.TattooMe.entity.FavoriteArtist;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface FavoriteArtistRepository extends JpaRepository<FavoriteArtist, UUID> {
     List<FavoriteArtist> findAllByUser_Id(UUID userId);
+
     boolean existsByUser_IdAndArtist_Id(UUID userId, UUID artistId);
+
+    @Transactional
     void deleteByUser_IdAndArtist_Id(UUID userId, UUID artistId);
 }
