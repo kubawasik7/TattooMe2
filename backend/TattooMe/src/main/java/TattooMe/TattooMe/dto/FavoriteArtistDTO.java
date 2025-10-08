c23f2935a5f73ff0c1d3ae5a782353d4e40794f9
Kuba <jakubwasik752@gmail.com>
2025-10-08 14:33:18 +0200

    dodanie FavoriteArtistDTO

---
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/FavoriteArtistDTO.java
@@ -0,0 +1,16 @@
+package TattooMe.TattooMe.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class FavoriteArtistDTO {
+    private UUID artistId;
+    private String username;
+    private String description;
+}
