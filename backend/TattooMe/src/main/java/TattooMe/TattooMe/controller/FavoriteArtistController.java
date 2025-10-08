48b35b19fe1c933fe3b5287a978e44b6f390cd80
Kuba <jakubwasik752@gmail.com>
2025-10-08 14:34:32 +0200

    edycja FavoriteArtistController na ResponseEntity

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/controller/FavoriteArtistController.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/controller/FavoriteArtistController.java
@@ -2,8 +2,12 @@ package TattooMe.TattooMe.controller;
 
 
 import TattooMe.TattooMe.Security.CustomUserDetails;
-import TattooMe.TattooMe.entity.User;
+import TattooMe.TattooMe.dto.FavoriteArtistDTO;
+import TattooMe.TattooMe.dto.UserDTO;
 import TattooMe.TattooMe.service.FavoriteArtistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,26 +18,26 @@ import java.util.UUID;
 @RequestMapping("/api/favorites")
 @CrossOrigin(origins = "http://localhost:4200")
 public class FavoriteArtistController {
-    private final FavoriteArtistService favoriteArtistService;
-
-    public FavoriteArtistController(FavoriteArtistService service) {
-        this.favoriteArtistService = service;
-    }
+    @Autowired
+    private FavoriteArtistService favoriteArtistService;
 
     @GetMapping
-    public List<User> getFavorites(@AuthenticationPrincipal CustomUserDetails user) {
-        return favoriteArtistService.getFavorites(user.getId());
+    public ResponseEntity<List<FavoriteArtistDTO>> getFavorites(@AuthenticationPrincipal CustomUserDetails user) {
+        List<FavoriteArtistDTO> favorites = favoriteArtistService.getFavorites(user.getId());
+        return ResponseEntity.ok(favorites);
     }
 
     @PostMapping("/{artistId}")
-    public void addFavorite(@PathVariable UUID artistId,
-                            @AuthenticationPrincipal CustomUserDetails user) {
-        favoriteArtistService.addFavorite(user.getId(), artistId);
+    public ResponseEntity<FavoriteArtistDTO> addFavorite(@PathVariable UUID artistId,
+                                                         @AuthenticationPrincipal CustomUserDetails user) {
+        FavoriteArtistDTO favorite = favoriteArtistService.addFavorite(artistId, user.getId());
+        return ResponseEntity.status(HttpStatus.CREATED).body(favorite);
     }
 
     @DeleteMapping("/{artistId}")
-    public void removeFavorite(@PathVariable UUID artistId,
-                               @AuthenticationPrincipal CustomUserDetails user) {
+    public ResponseEntity<Void> removeFavorite(@PathVariable UUID artistId,
+                                               @AuthenticationPrincipal CustomUserDetails user) {
         favoriteArtistService.removeFavorite(user.getId(), artistId);
+        return ResponseEntity.noContent().build();
     }
 }
