c1aeabaed1ecfb954e2ff4fbadd4451fa7e4a1b2
Kuba <jakubwasik752@gmail.com>
2025-10-09 11:21:29 +0200

    edycja RegisterRequest

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/RegisterRequest.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/RegisterRequest.java
@@ -1,40 +1,13 @@
 package TattooMe.TattooMe.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class RegisterRequest {
     private String nickname;
     private String password;
     private String email;
     private String role;
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
