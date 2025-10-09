5a953be9ae3ca1f4f48f88ae3cd27c4d8cad782f
Kuba <jakubwasik752@gmail.com>
2025-10-09 11:20:50 +0200

    edycja AuthController

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/controller/AuthController.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/controller/AuthController.java
@@ -5,12 +5,12 @@ import TattooMe.TattooMe.Security.JwtUtil;
 import TattooMe.TattooMe.dto.LoginRequest;
 import TattooMe.TattooMe.dto.LoginResponse;
 import TattooMe.TattooMe.dto.RegisterRequest;
+import TattooMe.TattooMe.dto.RegisterResponse;
 import TattooMe.TattooMe.entity.User;
 import TattooMe.TattooMe.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,40 +21,25 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 @CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
-    private final UserService userService;
-    private final JwtUtil jwtUtil;
-    private final PasswordEncoder passwordEncoder;
-    private final AuthenticationManager authenticationManager;
-
     @Autowired
-    public AuthController(UserService userService,
-                          AuthenticationManager authenticationManager,
-                          JwtUtil jwtUtil,
-                          PasswordEncoder passwordEncoder) {
-        this.userService = userService;
-        this.jwtUtil = jwtUtil;
-        this.passwordEncoder = passwordEncoder;
-        this.authenticationManager = authenticationManager;
+    private UserService userService;
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-    }
     @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody RegisterRequest registerRequest) {
-        userService.registerUser(registerRequest);
-        return ResponseEntity.ok("User registered");
+    public ResponseEntity<RegisterResponse> registerUser(@RequestBody RegisterRequest registerRequest) {
+        RegisterResponse response = userService.registerUser(registerRequest);
+        return ResponseEntity.ok(response);
     }
+
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) {
         try {
-
             User user = userService.getByNickname(loginRequest.getNickname());
-            //To jest do testu - do wywalenia potem
-            if(user == null) {
-                System.out.println("Nie znaleziono użytkownika");
-            } else {
-                System.out.println("Znaleziono użytkownika: " + user);
-            }
             if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Bledny login lub haslo");
             }
 
             CustomUserDetails userDetails = new CustomUserDetails(user);
@@ -65,8 +50,7 @@ public class AuthController {
             String token = jwtUtil.generateToken(userDetails);
             return ResponseEntity.ok(new LoginResponse(token));
         } catch (AuthenticationException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Bledny login lub haslo");
         }
     }
-
 }
