fe6dc9afda37e1bf70756176eb9c73244db23cc1
Kuba <jakubwasik752@gmail.com>
2025-10-09 12:12:18 +0200

    edycja CreateOfferDTO

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/CreateOfferDTO.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/CreateOfferDTO.java
@@ -1,33 +1,13 @@
 package TattooMe.TattooMe.dto;
 
-import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+@Getter
+@Setter
 public class CreateOfferDTO {
     private LocalDateTime startDate;
     private LocalDateTime endDate;
     private String description;
-
-    public LocalDateTime getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDateTime startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDateTime getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDateTime endDate) {
-        this.endDate = endDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
