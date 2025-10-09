884ad96b5fdf3aac957b6c220a5fed52d3af45a6
Kuba <jakubwasik752@gmail.com>
2025-10-09 11:21:15 +0200

    edycja LoginRequest

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/LoginRequest.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/LoginRequest.java
@@ -1,22 +1,11 @@
 package TattooMe.TattooMe.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginRequest {
     private String nickname;
     private String password;
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
