010e3b769f5d5c5e66a68050e93a874df339a011
Kuba <jakubwasik752@gmail.com>
2025-10-10 14:10:55 +0200

    edycja userProfileComponent

--- frontend/TattooMe/src/app/component/user-profile/user-profile.component.ts
+++ frontend/TattooMe/src/app/component/user-profile/user-profile.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../../service/user.service';
 import { User } from '../../model/user';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
@@ -11,30 +12,59 @@ import { User } from '../../model/user';
 })
 export class UserProfileComponent {
   user!: User;
-  userId!: string;
-  originalUser!: User;
+  userForm!: FormGroup;
   editMode = false;
+  selectedFile: any;
 
-  constructor(private route: ActivatedRoute, private userService: UserService){}
+  constructor(private route: ActivatedRoute, private userService: UserService, private fb: FormBuilder) { }
 
-  ngOnInit(){
-     const userId = this.route.snapshot.paramMap.get('id')!;
-
-    this.userService.getUserById(userId).subscribe(user => {
-      this.user = {...user};
-      this.originalUser = {...user};
+  ngOnInit() {
+    const userId = this.route.snapshot.paramMap.get('id')!;
+    this.userService.getUserById(userId).subscribe((data: User) => {
+      this.user = data;
+      this.initializeForm();
     });
   }
-    saveChanges(): void {
-    this.userService.updateUser(this.user).subscribe(() => {
-      this.originalUser = { ...this.user };
-      this.editMode = false;
+
+  initializeForm(): void {
+    this.userForm = this.fb.group({
+      nickname: [this.user.nickname, [Validators.required, Validators.minLength(3), Validators.maxLength(25)]],
+      name: [this.user.name, [Validators.required, Validators.minLength(3), Validators.maxLength(35)]],
+      surname: [this.user.surname, [Validators.required, Validators.minLength(3), Validators.maxLength(60)]],
+      email: [this.user.email, [Validators.required, Validators.email, Validators.maxLength(254)]]
     });
   }
-   cancelChanges(): void {
-    this.user = { ...this.originalUser };
+
+  get nickname() { return this.userForm?.get('nickname')!; }
+  get name() { return this.userForm?.get('name')!; }
+  get surname() { return this.userForm?.get('surname')!; }
+  get email() { return this.userForm?.get('email')!; }
+
+  toggleEdit(): void {
+    this.editMode = true;
+  }
+
+  cancelChanges(): void {
+    this.userForm.patchValue(this.user);
     this.editMode = false;
   }
 
+  saveChanges(): void {
+    if (this.userForm.invalid) {
+      this.userForm.markAllAsTouched();
+      return;
+    }
 
+    const updatedUser = { ...this.user, ...this.userForm.value };
+    this.userService.updateUser(updatedUser).subscribe({
+      next: () => {
+        this.user = updatedUser;
+        this.editMode = false;
+      }
+    });
+  }
+
+  onFileSelected(event: any): void {
+    this.selectedFile = event.target.files[0];
+  }
 }
