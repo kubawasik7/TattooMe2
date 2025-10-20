9daafa158e02b5f13dbbaef8d8552d7371796fc7
Kuba <jakubwasik752@gmail.com>
2025-10-20 14:45:09 +0200

    Dodanie notificationSevice dla snackBarow

---
+++ frontend/TattooMe/src/app/service/notification.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  constructor(private snackBar: MatSnackBar) { }
+
+  showSuccess(message: string) {
+    this.snackBar.open(message, 'Zamknij', {
+      duration: 4000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: ['snackbar-success']
+    });
+  }
+
+  showError(message: string) {
+    this.snackBar.open(message, 'Zamknij', {
+      duration: 4000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: ['snackbar-error']
+    });
+  }
+
+  showInfo(message: string) {
+    this.snackBar.open(message, 'Zamknij', {
+      duration: 4000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      panelClass: ['snackbar-info']
+    });
+  }
+}
