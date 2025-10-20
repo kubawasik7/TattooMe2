9801f92157046d5da8bbb486a852bf09002e6451
Kuba <jakubwasik752@gmail.com>
2025-10-20 10:12:41 +0200

    usuniecie zabezpieczenia z dto PersonInfoDTO

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/personInfo/PersonInfoDTO.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/personInfo/PersonInfoDTO.java
@@ -1,22 +1,14 @@
 package TattooMe.TattooMe.dto.personInfo;
 
-import jakarta.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class PersonInfoDTO {
-
-    @Size(min = 0, max = 255, message = "Pole może mieć maksymalnie 255 znaków")
     private String allergies;
-
-    @Size(min = 0, max = 255, message = "Pole może mieć maksymalnie 255 znaków")
     private String chronicDiseases;
-
-    @Size(min = 0, max = 255, message = "Pole może mieć maksymalnie 255 znaków")
     private String medicines;
-
-    @Size(min = 0, max = 255, message = "Pole może mieć maksymalnie 255 znaków")
     private String experiences;
 }
