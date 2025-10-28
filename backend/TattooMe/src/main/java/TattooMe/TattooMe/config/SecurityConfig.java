66a8010e296729708d0e6b97c3bf341768d70b54
Kuba <jakubwasik752@gmail.com>
2025-10-28 12:15:38 +0100

    dodanie endpointu do SecurityConfig

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/config/SecurityConfig.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/config/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig {
         http.authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
-                .requestMatchers(HttpMethod.GET,"/api/studios").permitAll()
+                .requestMatchers(HttpMethod.GET,"/api/studios/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/users/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/portfolio/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/flashes/**").permitAll()
