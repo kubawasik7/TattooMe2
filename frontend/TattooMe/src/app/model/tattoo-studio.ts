97ddb72d5025de5c51c319418c187d45302e4b71
Kuba <jakubwasik752@gmail.com>
2025-10-28 12:21:21 +0100

    dodanie pol do model tattooStudi

--- frontend/TattooMe/src/app/model/tattoo-studio.ts
+++ frontend/TattooMe/src/app/model/tattoo-studio.ts
@@ -1,7 +1,11 @@
+import { UUID } from "crypto";
+
 export interface TattooStudio {
+  id: UUID,
   name: string;
   city: string;
   street: string;
   streetNumber: string;
   postalCode: string;
+  ownerNickname: string;
 }
