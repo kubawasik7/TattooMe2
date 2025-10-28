3990cd0ab0ab07449289c5a95d812669ba6104ca
Kuba <jakubwasik752@gmail.com>
2025-10-28 12:21:00 +0100

    impl studio service frontend

--- frontend/TattooMe/src/app/service/studio.service.ts
+++ frontend/TattooMe/src/app/service/studio.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CreateTattoStudio } from '../model/create-tattoo-studio';
 import { TattooStudio } from '../model/tattoo-studio';
+import { User } from '../model/user';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,25 @@ import { TattooStudio } from '../model/tattoo-studio';
 export class StudioService {
   private baseUrl = 'http://localhost:8080/api/studios';
 
-  constructor(private http: HttpClient) {}
-  
-  getStudios(): Observable<TattooStudio[]>{
+  constructor(private http: HttpClient) { }
+
+  getStudioById(id: string): Observable<TattooStudio> {
+    return this.http.get<TattooStudio>(`${this.baseUrl}/${id}`);
+  }
+
+  getUsersByStudioId(studioId: string): Observable<User[]> {
+    return this.http.get<User[]>(`${this.baseUrl}/${studioId}/users`);
+  }
+
+  addUserToStudio(studioId: string, nickname: string): Observable<void> {
+    return this.http.post<void>(`${this.baseUrl}/${studioId}/users/by-nickname?nickname=${nickname}`, {});
+  }
+
+  removeUserFromStudio(studioId: string, userId: string): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${studioId}/users/${userId}`);
+  }
+
+  getStudios(): Observable<TattooStudio[]> {
     return this.http.get<TattooStudio[]>(this.baseUrl);
   }
 
