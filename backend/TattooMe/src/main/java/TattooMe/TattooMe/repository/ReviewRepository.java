5e254b0be9cfc600fe11c7c753e7397ff4366325
Kuba <jakubwasik752@gmail.com>
2025-10-29 17:52:20 +0100

    dodanie metod ReviewRepository

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/repository/ReviewRepository.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/repository/ReviewRepository.java
@@ -2,6 +2,8 @@ package TattooMe.TattooMe.repository;
 
 import TattooMe.TattooMe.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,7 +13,13 @@ import java.util.UUID;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
     List<Review> findByTarget_Id(UUID userId);
+
     List<Review> findByTattooStudio_Id(UUID studioId);
+
     Optional<Review> findByVisit_Id(UUID visitId);
 
+    @Query("SELECT AVG(r.rate) FROM Review r WHERE r.target.id = :userId")
+    Optional<Double> findAverageByTargetId(@Param("userId") UUID userId);
+
+    Long countByTargetId(UUID id);
 }
