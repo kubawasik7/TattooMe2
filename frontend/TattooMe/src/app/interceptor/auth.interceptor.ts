5e9b166ce270e1399bec9b0859fcb603a3af5b82
Kuba <jakubwasik752@gmail.com>
2025-11-15 15:42:20 +0100

    poprawa authInterceptor

--- frontend/TattooMe/src/app/interceptor/auth.interceptor.ts
+++ frontend/TattooMe/src/app/interceptor/auth.interceptor.ts
@@ -1,19 +1,35 @@
-import { HttpHandler, HttpInterceptor, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, catchError, throwError } from 'rxjs';
+import { NotificationService } from '../service/notification.service';
 @Injectable()
+
 export class AuthInterceptor implements HttpInterceptor {
- intercept(req: HttpRequest<any>, next: HttpHandler) {
-  let authReq = req;
-  if (typeof window !== 'undefined' && window.localStorage) {
-    const token = window.localStorage.getItem('token');
-    console.log('[AuthInterceptor] token=', token);
-    if (token) {
-      authReq = req.clone({
-        setHeaders: { Authorization: `Bearer ${token}` }
-      });
-      console.log('[AuthInterceptor] authReq.headers=', authReq.headers);
+
+  constructor(private router: Router, private notification: NotificationService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let authReq = req;
+
+    if (typeof window !== 'undefined' && window.localStorage) {
+      const token = window.localStorage.getItem('token');
+      if (token) {
+        authReq = req.clone({
+          setHeaders: { Authorization: `Bearer ${token}` }
+        });
+      }
     }
+
+    return next.handle(authReq).pipe(
+      catchError((err: any) => {
+        if (err instanceof HttpErrorResponse && err.status === 401) {
+          localStorage.removeItem('token');
+          this.notification.showError('Twoja sesja wygasła. Zaloguj się ponownie.');
+          this.router.navigate(['/login']);
+        }
+        return throwError(() => err);
+      })
+    );
   }
-  return next.handle(authReq);
-}
 }
