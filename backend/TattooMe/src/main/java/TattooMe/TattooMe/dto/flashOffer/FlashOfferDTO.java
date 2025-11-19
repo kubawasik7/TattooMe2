90144cd94bcc85fe7c1fef233109957e324fc28c
Kuba <jakubwasik752@gmail.com>
2025-11-19 12:48:56 +0100

    zabezpieczenie rabat w backend

--- backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/flashOffer/FlashOfferDTO.java
+++ backend/TattooMe/src/main/java/TattooMe/TattooMe/dto/flashOffer/FlashOfferDTO.java
@@ -1,6 +1,8 @@
 package TattooMe.TattooMe.dto.flashOffer;
 
 import TattooMe.TattooMe.dto.flash.FlashDTO;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +13,8 @@ import java.util.UUID;
 public class FlashOfferDTO {
     private UUID id;
     private String description;
+    @Min(value = 0, message = "Rabat nie może być mniejszy niż 0%")
+    @Max(value = 100, message = "Rabat nie może być większy niż 100%")
     private Integer percentOff;
     private UUID flashId;
     private UUID tattooArtistOfferId;
