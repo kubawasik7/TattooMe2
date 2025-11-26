cae07d878004015dc96fb25fc1f20bcc92d86048
Kuba <jakubwasik752@gmail.com>
2025-11-26 21:05:07 +0100

    poprawa tattooStudioOffer

--- frontend/TattooMe/src/app/component/tattoo-studio/tattoo-studio-offer/tattoo-studio-offer.component.ts
+++ frontend/TattooMe/src/app/component/tattoo-studio/tattoo-studio-offer/tattoo-studio-offer.component.ts
@@ -1,13 +1,12 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
 import { FlashService } from '../../../service/flash.service';
-import { NotificationService } from '../../../service/notification.service';
 import { TattooStudioOffer } from '../../../model/tattoo-studio-offer';
 import { Flash } from '../../../model/flash';
 import { TattooStudioOfferService } from '../../../service/tattoo-studio-offer.service';
-import { error } from 'console';
 import { FlashOffer } from '../../../model/flash-offer';
 import { FlashOfferService } from '../../../service/flash-offer.service';
+import Swal from 'sweetalert2';
+import { NotificationService } from '../../../service/notification.service';
 
 @Component({
   selector: 'app-tattoo-studio-offer',
@@ -16,21 +15,22 @@ import { FlashOfferService } from '../../../service/flash-offer.service';
   styleUrl: './tattoo-studio-offer.component.css'
 })
 export class TattooStudioOfferComponent implements OnInit {
- 
   @Input() studioId!: string;
   @Input() currentUserRole: string | null = null;
-    @Input() isOwner = false; 
 
   offers: TattooStudioOffer[] = [];
   flashes: Flash[] = [];
   flashOffersMap: Record<string, FlashOffer | null> = {};
   todayDate!: string;
 
+  editingOffer: TattooStudioOffer | null = null;
+
   constructor(
     private studioOfferService: TattooStudioOfferService,
     private flashService: FlashService,
-    private flashOfferService: FlashOfferService
-  ) {}
+    private flashOfferService: FlashOfferService,
+    private notification: NotificationService
+  ) { }
 
   ngOnInit(): void {
     this.todayDate = new Date().toISOString().slice(0, 16);
@@ -41,7 +41,6 @@ export class TattooStudioOfferComponent implements OnInit {
     this.studioOfferService.getCombinedOffers(this.studioId).subscribe({
       next: offers => {
         this.offers = offers;
-
         this.offers.forEach(o => {
           this.flashOfferService.getByArtistOffer(o.id).subscribe(flashOffers => {
             this.flashOffersMap[o.id] = flashOffers.length ? flashOffers[0] : null;
@@ -56,4 +55,122 @@ export class TattooStudioOfferComponent implements OnInit {
       error: err => console.error(err)
     });
   }
+
+  startAddOffer(): void {
+    this.editingOffer = {
+      id: '',
+      startDate: this.todayDate,
+      endDate: this.todayDate,
+      description: '',
+      name: 'Studio'
+    };
+  }
+
+  startEditOffer(offer: TattooStudioOffer): void {
+    if (!(this.currentUserRole === 'OWNER' || this.currentUserRole === 'EDITOR')) return;
+    if (offer.name !== 'Studio') return;
+    this.editingOffer = { ...offer };
+  }
+
+  cancelEdit(): void {
+    this.editingOffer = null;
+  }
+
+  isFormValid(): boolean {
+    if (!this.editingOffer) return false;
+
+    const start = new Date(this.editingOffer.startDate);
+    const end = new Date(this.editingOffer.endDate);
+    const now = new Date();
+
+    return !!this.editingOffer.startDate &&
+      !!this.editingOffer.endDate &&
+      !!this.editingOffer.description &&
+      this.editingOffer.description.length >= 5 &&
+      this.editingOffer.description.length <= 500 &&
+      start >= now &&
+      end >= start;
+  }
+
+  isStartDateValid(): boolean {
+    if (!this.editingOffer?.startDate) return false;
+    const now = new Date();
+    const start = new Date(this.editingOffer.startDate);
+    return start >= now;
+  }
+
+  saveOffer(): void {
+    if (!this.editingOffer || !this.isFormValid()) return;
+
+    //aktualizacja promocji
+    if (this.editingOffer.id) {
+      this.studioOfferService.updateOffer(this.studioId, this.editingOffer.id, this.editingOffer)
+        .subscribe({
+          next: updated => {
+            const idx = this.offers.findIndex(o => o.id === updated.id);
+            if (idx !== -1) this.offers[idx] = updated;
+
+            this.notification.showSuccess("Promocja została zaktualizowana");
+            this.editingOffer = null;
+          },
+          error: err => {
+            console.error(err);
+            this.notification.showError("Nie udało się zaktualizować promocji");
+          }
+        });
+
+      return;
+    }
+    //tworzenie nowej promocji
+    this.studioOfferService.createOffer(this.studioId, this.editingOffer)
+      .subscribe({
+        next: created => {
+          this.offers.push(created);
+          this.editingOffer = null;
+          this.notification.showSuccess("Promocja została dodana");
+        },
+        error: err => {
+          console.error(err);
+          this.notification.showError("Nie udało się dodać promocji");
+        }
+      });
+  }
+
+  deleteOffer(offer: TattooStudioOffer): void {
+    if (!(this.currentUserRole === 'OWNER' || this.currentUserRole === 'EDITOR')) return;
+    if (offer.name !== 'Studio') return;
+
+    Swal.fire({
+      title: 'Usunąć tę promocje?',
+      text: 'Tej akcji nie można cofnąć.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6',
+      background: '#1e1e1e',
+      color: '#ffffffff',
+      confirmButtonText: 'Tak, usuń',
+      cancelButtonText: 'Anuluj'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.studioOfferService.deleteOffer(this.studioId, offer.id).subscribe({
+          next: () => {
+            this.offers = this.offers.filter(o => o.id !== offer.id),
+              Swal.fire({
+                icon: 'success',
+                background: '#1e1e1e',
+                color: '#ffffffff',
+                title: 'Usunięto!',
+                timer: 1500,
+                showConfirmButton: false
+              });
+          },
+          error: (err) => {
+            this.notification.showError('Nie udało się usunąć promocji');
+          }
+        });
+      }
+    });
+  }
 }
+
